a1b7c9e8254e367a8f279ae39239a94591bfbf72
Cole Delong <coledelong421@gmail.com>
2020-02-09 19:33:44 -0600

    LCD and calculations added
    - Added LCD support
    - Added calculated formulas for temerature from voltage
    - Improved precision by taking the average of multiple voltages taken

--- Detector_Building/Detector_Building.ino
+++ Detector_Building/Detector_Building.ino
@@ -1,11 +1,19 @@
 // Cole Delong
 // 12/26/2019
 
+// Included pckages
+#include <LiquidCrystal.h>
+
+// LCD initialization
+LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
+
 // Pin constants
 const int bluePin = 5;
 const int greenPin = 4;
 const int redPin = 3;
 const int tempPin = 0;
+const int contrastPin = 6;
+const int buttonPin = 7;
 
 // Temperature ranges
 const int redUpper = 30;
@@ -15,6 +23,16 @@ const int greenLower = 10;
 const int blueUpper = 10;
 const int blueLower = 0;
 
+// ughhh average temp for more consistency
+const int resetNum = 400;
+float volts[resetNum];
+int cycles = 0;
+
+// jus button tings
+bool output = true;
+bool curButton;
+bool prevButton = false;
+
 void setup() {
 
   // Initialize the LED pins
@@ -22,7 +40,13 @@ void setup() {
   pinMode(greenPin, OUTPUT);
   pinMode(redPin, OUTPUT);
 
-  // Start a serial port so we can see the temp
+  // Initialize the button pin
+  pinMode(buttonPin, INPUT);
+  // Initialize the LCD screen
+  analogWrite(6, 75);
+  lcd.begin(16, 2);
+
+  // Start a serial port
   Serial.begin(9600);
 
 }
@@ -35,30 +59,68 @@ void loop() {
   // Find the voltage the tempPin is receiving
   voltage = getVoltage();
 
-  // CALIBRATE!!! Use linear equation derived from data to determine the degrees celcius based off of the voltage
-  tempC = (voltage * 100) - 50;
+  // only do calculations every 100 cycles with the average of the cycles
+  //Serial.println(cycles); 
+  volts[cycles] = voltage;
+  //Serial.println(volts[cycles]);
+  
+  if (cycles >= resetNum - 1) {
 
-  // Use the calculated celcius to determine the temp feihrenheaeiiit because this is America
-  tempF = tempC * (9.0/5.0) + 32.0;
+    // Reset the counter
+    cycles = 0;
 
-  // Print both of these values to the serial port. Eventually these will be displayed on an LCD screen.
-  Serial.print("voltage: ");
-  Serial.print(voltage);
-  Serial.print("  temp C: ");
-  Serial.print(tempC);
-  Serial.print("  temp F: ");
-  Serial.println(tempF);
+    // Use the average of the voltages as the voltage
+    float sum = 0;
+    for (int i = 0; i < resetNum; i++) {
+      sum += volts[i];
+    }
+    voltage = sum/resetNum;
 
-  // Turn on the correct LED for the temperature range
-  digitalWrite(redPin, LOW);
-  digitalWrite(greenPin, LOW);
-  digitalWrite(bluePin, LOW);
-  if (tempC <= redUpper && tempC >= redLower) digitalWrite(redPin, HIGH);
-  if (tempC <= greenUpper && tempC >= greenLower) digitalWrite(greenPin, HIGH);
-  if (tempC <= blueUpper && tempC >= blueLower) digitalWrite(bluePin, HIGH);
+    // Use the calculated celcius to determine the temp feihrenheaeiiit because this is America
+    tempF = 173.975 * voltage - 61.2073;
 
-  // Delay so it doesn't go too crazy
-  delay(500);
+    // Use linear equation derived from data to determine the degrees celcius based off of the voltage
+    tempC = (tempF - 32) * (5.0/9.0);
+  
+    // If the output is set to on, output the data
+    if (output) {
+      // Print both of these values to the serial port and to the LCD screen
+      Serial.print("voltage: ");
+      Serial.print(voltage, 5);
+      Serial.print("  temp C: ");
+      Serial.print(tempC);
+      Serial.print("  temp F: ");
+      Serial.println(tempF);
+      lcd.setCursor(0, 0);
+      lcd.print("Voltage: "); 
+      lcd.print(voltage, 5);
+      lcd.setCursor(0,1);
+      lcd.print("Deg C: ");
+      lcd.print(tempC);
+      
+  
+      // Turn on the correct LED for the temperature range
+      digitalWrite(redPin, LOW);
+      digitalWrite(greenPin, LOW);
+      digitalWrite(bluePin, LOW);
+      if (tempC <= redUpper && tempC >= redLower) digitalWrite(redPin, HIGH);
+      if (tempC <= greenUpper && tempC >= greenLower) digitalWrite(greenPin, HIGH);
+      if (tempC <= blueUpper && tempC >= blueLower) digitalWrite(bluePin, HIGH);
+      
+    }
+  }
+
+  // Pause the screen if the button is pressed
+  curButton = digitalRead(buttonPin);
+  if (curButton != prevButton) toggleOutput();
+  prevButton = curButton;
+
+
+  // increment the cycle count
+  cycles += 1;
+
+  // Give it a break, it's been working hard.
+  delay(2);
 
 }
 
@@ -67,4 +129,12 @@ float getVoltage() {
   // Calculate the voltage by changing it from a range of 0 to 1023 to a range of 0 to 5. Return the result.
   return (analogRead(tempPin) * (5.0 / 1024.0));
   
+}
+
+void toggleOutput() {
+
+  // toggle the toggleOutput var
+  if (output) output = false;
+  else output = true;
+
 }
